a2f365ab9aa79c0217e1e099c6853836da10b392
Michele Basile <mich.basile@gmail.com>
2016-11-27 11:43:48 +0100

    Initial Commit

---
+++ FlightCompany/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlightCompany
+//
+//  Created by Michele Basile on 27/11/16.
+//  Copyright © 2016 Deadliners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
