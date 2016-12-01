6d9253e6dbf2db10f5439a9e2b529254b77cdca3
Michele Basile <mich.basile@gmail.com>
2016-12-01 11:23:42 +0100

    login screen

---
+++ FlightCompany/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  FlightCompany
+//
+//  Created by Michele Basile on 01/12/16.
+//  Copyright © 2016 Deadliners. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func signInDidPress(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signUpDidPress(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: - Controller lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
